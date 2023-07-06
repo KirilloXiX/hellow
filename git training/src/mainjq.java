1ffd30e74e3bcaf08dc34ac65d8095d3694378a2
Kirill <vovk.kiryusha.007@gmail.com>
2023-07-06 18:41:53 +0300

    adding for new main

--- git training/src/mainjq.java
+++ git training/src/mainjq.java
@@ -2,6 +2,7 @@ public class mainjq {
     public static void main(String[] args){
         System.out.println("Hello rev");
         System.out.println("Also helo");
+        System.out.println("Addin for new main");
 
     }
 }
