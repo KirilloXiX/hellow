612d8aafd13190fb30e23f619dc165c551a12b8f
Kirill <vovk.kiryusha.007@gmail.com>
2023-07-06 18:12:28 +0300

    Dobavil hello mate

--- git training/src/mainjq.java
+++ git training/src/mainjq.java
@@ -1,5 +1,7 @@
 public class mainjq {
     public static void main(String[] args){
-        System.out.println("Hello reviewers");
+        System.out.println("Hello reviewer");
+        System.out.println("Hello mate");
+
     }
 }
